7722cad54e49a240ec46220b1fb7e5f05ca8f5c8
Geoffrey Gowan <geoffrey.gowan@gmail.com>
2016-04-04 22:11:15 -0700

    Copying review adjustments from prior year

--- admin.js
+++ admin.js
@@ -64,12 +64,154 @@ function createParishFolders($scope) {
   shared.initDriveApi(function() {createFoldersAuthorized($scope);});
 }
 
-function copyPropertiesIfPresent(source, dest, properties) {
+// Copies a single property from the formSource to the dest object. If reviewSource is provided,
+// it will override formSource if it contains a number. sourceName is the name of the property
+// to copy in the sources. destName is the name of the property to copy to on the dest object.
+// If destName is not provided, it defaults to the same as sourceName.
+function copyPropertyIfPresent(formSource, reviewSource, dest, sourceName, destName) {
+  if (!destName) {
+    destName = sourceName;
+  }
+  if (reviewSource && sourceName in reviewSource && angular.isNumber(reviewSource[sourceName])) {
+    // review can override form only when review contains a number.
+    dest[destName] = reviewSource[sourceName];
+  } else if (sourceName in formSource) {
+    dest[destName] = formSource[sourceName];
+  }
+}
+
+// Copies specified properties from the formSource to the dest object. If reviewSource is provided,
+// it will override formSource if it contains a number (and not just a string, empty or otherwise).
+function copyPropertiesIfPresent(formSource, reviewSource, dest, properties) {
   angular.forEach(properties, function(property) {
-    if (source[property]) {
-      dest[property] = source[property];
-    }
+    copyPropertyIfPresent(formSource, reviewSource, dest, property);
   });
+}
+
+function copyDataIfReady(parishId, parishInfo, previousFormVal, previousReviewVal, currentFormRef) {
+  if (!previousFormVal || !previousReviewVal) {
+    return;
+  }
+  console.log("have previous form data", parishId, previousFormVal);
+  var transactionMessage = null;
+  currentFormRef.transaction(function(currentFormVal) {
+    console.log("transaction update function", parishId, currentFormVal);
+    if (currentFormVal) {
+      console.log("recording that parish already has data", parishId);
+      transactionMessage = "parish already has data; not copying";
+      return currentFormVal;
+    }
+    transactionMessage = null;
+    currentFormVal = {};
+    copyPropertiesIfPresent(previousFormVal, null, currentFormVal, [
+      'priest_name',
+      'priest_phone',
+      'priest_email',
+      'pres_name',
+      'pres_phone',
+      'pres_email',
+      'treas_name',
+      'treas_phone',
+      'treas_email',
+      'preparer_name',
+      'preparer_phone',
+      'preparer_email',
+      'income_explanation',
+      'expense_explanation',
+      'nmc_lines',
+      'arch_don_lines',
+      'auth_min_lines',
+      'metro_lines',
+      'metro_desc',
+      'patriarch_lines',
+      'patriarch_desc',
+      'cap_lines',
+      'cap_projects',
+      'const_loan_lines',
+      'mort_lines',
+      'fundraising_lines',
+      'school_lines',
+      'religious_ed_lines',
+      'catastrophic_lines',
+      'moving_lines',
+      'outreach_lines',
+      'clergy_laity_lines',
+      'other_hier_lines',
+      'other_hier_explanation',
+      'dues_family',
+      'dues_single',
+      'dues_senior',
+      'stew_name1',
+      'stew_phone1',
+      'stew_email1',
+      'stew_name2',
+      'stew_phone2',
+      'stew_email2',
+      'stew_name3',
+      'stew_phone3',
+      'stew_email3',
+      'stew_name4',
+      'stew_phone4',
+      'stew_email4',
+    ]);
+    if (previousFormVal.Y2014) {
+      if (!currentFormVal.Y1) {
+        currentFormVal.Y1 = {};
+      }
+      copyPropertiesIfPresent(previousFormVal.Y2014, previousReviewVal.Y2014, currentFormVal.Y1, [
+        'income',
+        'expenses',
+        'nmc',
+        'metro',
+        'patriarch',
+        'cap',
+        'const_loan',
+        'mort',
+        'fundraising',
+        'school',
+        'religious_ed',
+        'moving',
+        'outreach',
+        'clergy_laity',
+        'other_hier',
+        'prop_liab'
+      ]);
+      copyPropertyIfPresent(previousFormVal.Y2014, previousReviewVal.Y2014,
+          currentFormVal.Y1, 'unusual', 'catastrophic');
+      var arch = shared.sumFields(shared.ARCH_MIN_FIELDS, previousReviewVal.Y2014,
+          previousFormVal.Y2014);
+      if (arch) {
+        currentFormVal.Y1.arch = arch;
+      }
+      var authMin = shared.sumFields(shared.AUTH_MIN_FIELDS, previousReviewVal.Y2014,
+          previousFormVal.Y2014);
+      if (authMin) {
+        currentFormVal.Y1.auth_min = authMin;
+      }
+      copyPropertiesIfPresent(previousFormVal.Y2014, previousReviewVal.Y2014,
+          currentFormVal.Y1, shared.STEWARDSHIP_FIELDS_PER_YEAR);
+      if (!currentFormVal.Y2) {
+        currentFormVal.Y2 = {};
+      }
+      copyPropertiesIfPresent(currentFormVal.Y1, null, currentFormVal.Y2, 
+          ['stew_or_dues', 'how_counted']);
+    }
+    if (previousFormVal.Y2013) {
+      if (!currentFormVal.Y0) {
+        currentFormVal.Y0 = {};
+      }
+      copyPropertiesIfPresent(previousFormVal.Y2013, null, currentFormVal.Y0,
+          shared.STEWARDSHIP_FIELDS_PER_YEAR);
+    }
+    return currentFormVal;
+  }, function(error, wasCommitted, finalSnapshot) {
+    if (error || !wasCommitted || transactionMessage) {
+      console.log("Failed to copy data for parish", parishId, transactionMessage, error, 
+          wasCommitted);
+    } else {
+      console.log("Copied data for parish", parishId);
+    }
+  }, false);
 }
 
 function setupScope($scope, $firebaseObject) {
@@ -127,139 +269,27 @@ function setupScope($scope, $firebaseObject) {
         return;
       }
       console.log("parishId: ", parishId);
-      previousFormRef = $scope.metroRef.child("data-form/" + (shared.FOR_YEAR - 1) + "/parish/" + parishId);
+      var previousFormVal = null;
+      var previousReviewVal = null;
+      var currentFormRef = $scope.metroRef.child("data-form/" + shared.FOR_YEAR + "/parish/" + parishId);
+      var previousFormRef = $scope.metroRef.child("data-form/" + (shared.FOR_YEAR - 1) + "/parish/" + parishId);
       previousFormRef.once("value", function(previousFormSnapshot) {
-        console.log("once responded", parishId);
+        console.log("data form responded", parishId);
         previousFormVal = previousFormSnapshot.val();
-        if (previousFormVal) {
-          console.log("have previous form data", parishId, previousFormVal);
-          currentFormRef = $scope.metroRef.child("data-form/" + shared.FOR_YEAR + "/parish/" + parishId);
-          var transactionMessage = null;
-          currentFormRef.transaction(function(currentFormVal) {
-            console.log("transaction update function", parishId, currentFormVal);
-            if (currentFormVal) {
-              console.log("recording that parish already has data", parishId);
-              transactionMessage = "parish already has data; not copying";
-              return currentFormVal;
-            }
-            transactionMessage = null;
-            currentFormVal = {};
-            copyPropertiesIfPresent(previousFormVal, currentFormVal, [
-              'priest_name',
-              'priest_phone',
-              'priest_email',
-              'pres_name',
-              'pres_phone',
-              'pres_email',
-              'treas_name',
-              'treas_phone',
-              'treas_email',
-              'preparer_name',
-              'preparer_phone',
-              'preparer_email',
-              'income_explanation',
-              'expense_explanation',
-              'nmc_lines',
-              'arch_don_lines',
-              'auth_min_lines',
-              'metro_lines',
-              'metro_desc',
-              'patriarch_lines',
-              'patriarch_desc',
-              'cap_lines',
-              'cap_projects',
-              'const_loan_lines',
-              'mort_lines',
-              'fundraising_lines',
-              'school_lines',
-              'religious_ed_lines',
-              'catastrophic_lines',
-              'moving_lines',
-              'outreach_lines',
-              'clergy_laity_lines',
-              'other_hier_lines',
-              'other_hier_explanation',
-              'dues_family',
-              'dues_single',
-              'dues_senior',
-              'stew_name1',
-              'stew_phone1',
-              'stew_email1',
-              'stew_name2',
-              'stew_phone2',
-              'stew_email2',
-              'stew_name3',
-              'stew_phone3',
-              'stew_email3',
-              'stew_name4',
-              'stew_phone4',
-              'stew_email4',
-            ]);
-            if (previousFormVal.Y2014) {
-              if (!currentFormVal.Y1) {
-                currentFormVal.Y1 = {};
-              }
-              copyPropertiesIfPresent(previousFormVal.Y2014, currentFormVal.Y1, [
-                'income',
-                'expenses',
-                'nmc',
-                'metro',
-                'patriarch',
-                'cap',
-                'const_loan',
-                'mort',
-                'fundraising',
-                'school',
-                'religious_ed',
-                'moving',
-                'outreach',
-                'clergy_laity',
-                'other_hier',
-                'prop_liab'
-              ]);
-              if (previousFormVal.Y2014.unusual) {
-                currentFormVal.Y1.catastrophic = previousFormVal.Y2014.unusual;
-              }
-              var arch = shared.sumFields(shared.ARCH_MIN_FIELDS, previousFormVal.Y2014);
-              if (arch) {
-                currentFormVal.Y1.arch = arch;
-              }
-              var authMin = shared.sumFields(shared.AUTH_MIN_FIELDS, previousFormVal.Y2014);
-              if (authMin) {
-                currentFormVal.Y1.auth_min = authMin;
-              }
-              copyPropertiesIfPresent(previousFormVal.Y2014, currentFormVal.Y1,
-                shared.STEWARDSHIP_FIELDS_PER_YEAR);
-              if (!currentFormVal.Y2) {
-                currentFormVal.Y2 = {};
-              }
-              copyPropertiesIfPresent(currentFormVal.Y1, currentFormVal.Y2, 
-                ['stew_or_dues', 'how_counted']);
-            }
-            if (previousFormVal.Y2013) {
-              if (!currentFormVal.Y0) {
-                currentFormVal.Y0 = {};
-              }
-              copyPropertiesIfPresent(previousFormVal.Y2013, currentFormVal.Y0,
-                shared.STEWARDSHIP_FIELDS_PER_YEAR);
-            }
-            return currentFormVal;
-          }, function(error, wasCommitted, finalSnapshot) {
-            if (error || !wasCommitted || transactionMessage) {
-              console.log("Failed to copy data for parish", parishId, transactionMessage, error, 
-                  wasCommitted);
-            } else {
-              console.log("Copied data for parish", parishId);
-            }
-          }, false);
-        } else {
-          console.log("no previous form data", parishId);
-        }
+        copyDataIfReady(parishId, parishInfo, previousFormVal, previousReviewVal, currentFormRef);
       }, function(error) {
-        console.log("Couldn't read prior year's data for parish", parishId, error);
+        console.log("Couldn't read prior year's form for parish", parishId, error);
+      });
+      var previousReviewRef = $scope.metroRef.child("review-data/" + (shared.FOR_YEAR - 1) + "/parish/" + parishId);
+      previousReviewRef.once("value", function(previousReviewSnapshot) {
+        console.log("review responded", parishId);
+        previousReviewVal = previousReviewSnapshot.val();
+        copyDataIfReady(parishId, parishInfo, previousFormVal, previousReviewVal, currentFormRef);
+      }, function(error) {
+        console.log("Couldn't read prior year's review for parish", parishId, error);
       });
     });
-  };
+  }
 }
 
 app.controller("Ctrl", function($scope, $firebaseObject) {
